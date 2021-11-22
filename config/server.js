2b95f1715aa4e461e28d791e9c97a9e7d08c5e6e
Aaron Toliver <flux.charlotte@gmail.com>
2021-11-22 13:24:31 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'adbe7d086afcf70dbee5f0dd0c445afc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6c3186f2-5ffe-4ead-be23-de2d8d3a34c9'
+};
